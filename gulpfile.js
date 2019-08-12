403789c1789fbf008b80a10a4f541128013702d0
Cas Dijkman <cas@dijkman.xyz>
2019-08-12 21:47:16 +0200

    Add watch task for sass files

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const { src, dest, series, parallel } = require('gulp');
+const { src, dest, watch, series, parallel } = require('gulp');
 const sass = require('gulp-sass');
 const rename = require('gulp-rename');
 const del = require('del');
@@ -20,19 +20,24 @@ function compileSass() {
 	.pipe(dest(destination))
 }
 
-function compileMinifiedSass() {
+function compileSassMinified() {
     return src(globs.src.sass)
 	.pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
-	.pipe(rename({
-	    extname: ".min.css"
-	}))
+	.pipe(rename({ extname: ".min.css" }))
 	.pipe(dest(destination))
 }
+
+const compileSassAll = parallel(compileSass, compileSassMinified);
 
 function clean() {
     return del(destination)
 }
 
+function watchSass() {
+    return watch(globs.src.sass, compileSassAll)
+}
+
 module.exports = {
-    default: series(clean, parallel(compileSass, compileMinifiedSass))
+    build: series(clean, compileSassAll),
+    default: series(clean, compileSassAll, watchSass)
 }
