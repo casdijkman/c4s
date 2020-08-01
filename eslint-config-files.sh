d2a2dd17605ee42044e4b9232095e953ad17e54e
Cas Dijkman <cas@dijkman.xyz>
2020-08-01 15:14:13 +0200

    Move linting rc files to package.json

--- eslint-config-files.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-# This hacky script is needed because config files (dotfiles) are ignored by default
-# and there is apparently no way to counteract this in eslint-gulp.
-yarn eslint .*.js --ignore-pattern '!.*'
