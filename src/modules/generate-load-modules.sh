6b4d377a9cc865d8b6f9de47dee24a29121bc228
Cas Dijkman <cas@dijkman.xyz>
2021-10-22 00:29:10 +0200

    Add mixin that dynamically loads modules from configuration

---
+++ src/modules/generate-load-modules.sh
@@ -0,0 +1,78 @@
+#!/usr/bin/env bash
+
+#set -x # Enable debugging
+
+[[ $(dirname "$(realpath "$0")") != $(pwd) ]] && echo "Run script from it's own directory" && exit 1
+
+directories=(non-responsive responsive)
+
+if [[ $1 == --modules ]]; then
+    echo "\$modules: ("
+    for directory in "${directories[@]}"; do
+	for file in "${directory}"/_*.scss; do
+	    baseName=$(basename "$file")
+	    newFile="${baseName:1}" # Remove first character (being _)
+	    moduleName="${newFile/.scss/}"
+	    echo -n "  $moduleName: "
+	    if [[ $directory == responsive ]]; then
+		echo -n "true"
+	    else
+		echo -n "false"
+	    fi
+	    echo ","
+	done
+    done
+    echo ");"
+    exit 0
+fi
+
+echo "@use 'sass:map';"
+echo "@use '../variables';"
+
+for directory in "${directories[@]}"; do
+    for file in "${directory}"/_*.scss; do
+	baseName=$(basename "$file")
+	newFile="${baseName:1}" # Remove first character (being _)
+	moduleName="${newFile/.scss/}"
+
+	echo "@use './$directory/$moduleName';"
+    done
+done
+
+echo
+echo "@mixin load-modules(\$modules: null, \$breakpoints: null) {"
+echo "  @if \$modules == null {"
+echo "    \$modules: variables.\$modules;"
+echo "  }"
+echo
+echo "  @if \$breakpoints == null {"
+echo "    \$breakpoints: variables.\$breakpoints;"
+echo "  }"
+echo
+echo "  @each \$module, \$responsive in \$modules {"
+
+
+for directory in "${directories[@]}"; do
+    unset mixinArgument
+    [[ $directory == "responsive" ]] && mixinArgument="''"
+
+    for file in "${directory}"/_*.scss; do
+        baseName=$(basename "$file")
+	newFile="${baseName:1}" # Remove first character (being _)
+	moduleName="${newFile/.scss/}"
+
+	echo "    @if \$module == $moduleName {"
+	echo "      @include $moduleName.$moduleName($mixinArgument);"
+	echo
+	echo "      @if \$responsive {"
+	echo "        @each \$breakpoint, \$map in \$breakpoints {"
+	echo "          @include $moduleName.$moduleName(\$breakpoint);"
+	echo "        }"
+	echo "      }"
+	echo "    }"
+	echo
+    done
+done
+
+echo "  }"
+echo "}"
