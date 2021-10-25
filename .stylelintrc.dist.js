7653f5083793a9cd1c9eb0837651282bc42904c7
Cas Dijkman <cas@dijkman.xyz>
2021-10-26 01:03:28 +0200

    Update compilation pipeline
    Merge @media queries in css
    Pretty print css
    Add linting for compiled css
    Integrate gzipping into gulpfile

---
+++ .stylelintrc.dist.js
@@ -0,0 +1,13 @@
+module.exports = {
+    "extends": [ "./.stylelintrc.js"],
+    "rules": {
+	'at-rule-empty-line-before': null,
+	'rule-empty-line-before': null,
+	'number-leading-zero': 'always',
+	'string-quotes': 'double',
+	'max-line-length': 80,
+	'no-missing-end-of-source-newline': null,
+	'value-list-comma-newline-after': null,
+	'declaration-colon-newline-after': null
+    }
+};
--- gulpfile.js/index.js
+++ gulpfile.js/index.js
@@ -4,7 +4,12 @@ const { src, dest, watch, series, parallel } = require('gulp');
 const { getNunjucksData, manageEnv } = require('./nunjucks');
 const sass = require('gulp-sass')(require('sass'));
 sass.compiler = require('sass');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const csso = require('gulp-csso');
 const rename = require('gulp-rename');
+const prettier = require('gulp-prettier');
+const gzip = require('gulp-gzip');
 const del = require('del');
 const nunjucksRender = require('gulp-nunjucks-render');
 const data = require('gulp-data');
@@ -19,20 +24,34 @@ const globs = {
     src: {
 	sass:     './src/**/*.scss',
 	nunjucks: './docs/pages/**/*.njk'
+    },
+    dist: {
+	css:      ['./dist/**/*.css', '!./dist/**/*.min.css'],
+	cssAll:   './dist/**/*.css'
     }
+};
+
+const stylelintOptions = {
+    reporters: [
+	{ formatter: 'string', console: true }
+    ],
+    debug: true
 };
 
 function compileSass() {
     return src(globs.src.sass)
-	.pipe(sass({ sourceComments: true }).on('error', sass.logError))
-	.pipe(dest(destination));
-}
-
-function compileSassMinified() {
-    return src(globs.src.sass)
-	.pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+	.pipe(rename((path) => {
+	    path.basename = path.basename.replace(/-module$/, '');
+	}))
+	.pipe(sass.sync().on('error', sass.logError))
+	.pipe(postcss([ autoprefixer ]))
+	.pipe(csso({ forceMediaMerge: true }))
 	.pipe(rename({ extname: '.min.css' }))
-	.pipe(dest(destination));
+	.pipe(dest(destination))           // Output optimized css files as .min.css
+	.pipe(prettier())
+	.pipe(rename({ extname: '' }))     // Remove .css extension (from .min.css)
+	.pipe(rename({ extname: '.css' })) // Replace .min extension with .css
+	.pipe(dest(destination));          // Output pretty-printed optimized files as .css
 }
 
 function compileNunjucks() {
@@ -44,7 +63,7 @@ function compileNunjucks() {
 	    collapseWhitespace: true
 	}))
 	.pipe(beautifier({
-	    indent_size: 2, // eslint-disable-line camelcase
+	    indent_size: 2,        // eslint-disable-line camelcase
 	    end_with_newline: true // eslint-disable-line camelcase
 	}))
 	.pipe(dest(destination));
@@ -52,26 +71,34 @@ function compileNunjucks() {
 
 function lintSass() {
     return src(globs.src.sass)
-        .pipe(stylelint({
-            reporters: [
-                { formatter: 'string', console: true }
-            ]
-        }));
+	.pipe(stylelint(stylelintOptions));
+}
+
+function lintCss() {
+    const options = stylelintOptions;
+    options.config = require('../.stylelintrc.dist.js');
+    return src(globs.dist.css)
+	.pipe(stylelint(options));
 }
 
 function lintJs() {
-    return src(['**/*.js', '!node_modules/**', '!distribution/**'])
-        .pipe(eslint())
-        .pipe(eslint.format())
-        .pipe(eslint.failAfterError());
+    return src(['**/*.js', '!node_modules/**'])
+	.pipe(eslint())
+	.pipe(eslint.format())
+	.pipe(eslint.failAfterError());
+}
+
+function gzipDist() {
+    return src(globs.dist.cssAll)
+	.pipe(gzip())
+	.pipe(dest(destination));
 }
 
 function clean() {
     return del(destination);
 }
 
-const compileSassAll = parallel(compileSass, compileSassMinified);
-const build = series(clean, compileSassAll, compileNunjucks);
+const build = series(clean, compileSass, parallel(compileNunjucks, gzipDist));
 
 function watchFiles() {
     watch(globs.src.sass, build);
@@ -80,9 +107,9 @@ function watchFiles() {
 
 module.exports = {
     nunjucks: compileNunjucks,
-    sass: compileSassAll,
+    sass: compileSass,
     clean,
     build,
     default: series(build, watchFiles),
-    lint: parallel(lintSass, lintJs)
+    lint: parallel(lintSass, lintCss, lintJs)
 };
