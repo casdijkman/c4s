701e2400879c6f43cf7533c8b662b3b73051d317
Cas Dijkman <cas@dijkman.xyz>
2023-07-13 19:46:53 +0200

    Resolve stylelint suggestions for dist

--- .stylelintrc.dist.js
+++ .stylelintrc.dist.js
@@ -2,21 +2,31 @@
 //
 // SPDX-License-Identifier: GPL-3.0-only
 
+const selectorClassPattern =
+      '^(([a-z][a-z0-9]*)|(translate[XY]))(--?([a-z0-9]+|(translate[XY])))*$';
+
 module.exports = {
     extends: [ './.stylelintrc.js' ],
     rules: {
+        'alpha-value-notation': null,
         'at-rule-empty-line-before': null,
-        'rule-empty-line-before': null,
-        'number-leading-zero': 'always',
-        'string-quotes': 'double',
-        'max-line-length': null,
-        'no-missing-end-of-source-newline': null,
-        'value-list-comma-newline-after': null,
-        'declaration-colon-newline-after': null,
+        'color-function-notation': null,
         'comment-empty-line-before': null,
-        'no-duplicate-selectors': null,
-        'selector-list-comma-newline-after': null,
+        'declaration-colon-newline-after': null,
         'declaration-empty-line-before': null,
-        'indentation': null
+        'function-url-quotes': 'never',
+        'indentation': null,
+        'max-line-length': null,
+        'media-feature-range-notation': 'prefix',
+        'no-duplicate-selectors': null,
+        'no-missing-end-of-source-newline': null,
+        'number-max-precision': null,
+        'property-no-vendor-prefix': null,
+        'rule-empty-line-before': null,
+        'scss/operator-no-newline-after': null,
+        'scss/operator-no-unspaced': null,
+        'selector-class-pattern': selectorClassPattern,
+        'selector-list-comma-newline-after': null,
+        'value-list-comma-newline-after': null
     }
 };
