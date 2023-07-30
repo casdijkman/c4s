773448f6f890df2f8921f7599fe3a4d7ae064fb2
Cas Dijkman <cas@dijkman.xyz>
2023-07-30 17:28:15 +0200

    Fix script order in update-all script

--- src/update-all.sh
+++ src/update-all.sh
@@ -9,11 +9,11 @@
 [[ $(dirname "$(realpath "$0")") != $(pwd) ]] && echo "Run script from it's own directory" && exit 1
 
 scripts=(
+    update-version.sh
     update-custom-verbose.sh
-    update-load-modules-mixin.sh
     update-module-files.js
     update-module-list.sh
-    update-version.sh
+    update-load-modules-mixin.sh
 )
 
 for script in "${scripts[@]}"; do
