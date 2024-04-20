14fcb262c15963aa4ca8a825441a5a64391ce753
Cas Dijkman <cas@cdijkman.nl>
2024-04-20 21:20:55 +0200

    Add c4s-custom to update version script

--- src/update-version.sh
+++ src/update-version.sh
@@ -8,14 +8,19 @@
 
 [[ $(dirname "$(realpath "$0")") != $(pwd) ]] && echo "Run script from it's own directory" && exit 1
 
-variables_file=./_variables.scss
-content=$(cat "$variables_file")
+files=(
+    ./_variables.scss
+    ./c4s-custom.scss
+)
 
-{
-    version=$(jq --raw-output ".version" <../package.json)
-    search='C4S CSS, version .*'
-    replace="C4S CSS, version ${version} <https:\/\/c4s.cdijkman.nl>"
-    # shellcheck disable=SC2001
-    sed "s/${search}/${replace}/" <<<"$content"
+for file in "${files[@]}"; do
+    content=$(cat "$file")
 
-} >$variables_file
+    {
+        version=$(jq --raw-output ".version" <../package.json)
+        search='C4S CSS, version .*'
+        replace="C4S CSS, version ${version} <https:\/\/c4s.cdijkman.nl>"
+        # shellcheck disable=SC2001
+        sed "s/${search}/${replace}/" <<<"$content"
+    } >"$file"
+done
